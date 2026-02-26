f5bd250825b454f536a28cd0aa971c8ffe54e98e
KierllosMedhat <kierllosmedhat@outlook.com>
2026-02-26 20:52:47 +0200

    initial project structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.social_application {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.social_application to javafx.fxml;
+    exports com.example.social_application;
+}
