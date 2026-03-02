ea621ac9ec89a876231b3be1788e72f8f541ef5a
KierllosMedhat <kierllosmedhat@outlook.com>
2026-03-02 19:54:51 +0200

    database & models

---
+++ src/main/java/models/Profile.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class Profile {
+    private int  id;
+    private int userId;
+    private String name;
+    private String bio;
+    private String profilePicture; // Path
+    private LocalDateTime updatedAt;
+
+    public Profile(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,64 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int id;
+    private String email;
+    private String passwordHash;
+    private LocalDateTime createdAt;
+    private Profile profile;
+
+    public User() {
+    }
+
+    public User(String email, String passwordHash) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", email='" + email + "'}";
+    }
+}
