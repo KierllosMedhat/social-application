1af432ace467f9589b1235d577ea68b22c188fb3
Abanoub Nashat <abanoub2100065@fci.sohag.edu.eg>
2026-03-04 01:44:12 +0200

    added configs to work on vscode

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.social_application {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires transitive javafx.graphics;
 
     opens com.example.social_application to javafx.fxml;
     exports com.example.social_application;
